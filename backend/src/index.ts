6b10d4378e71e63f46aef8e60173a14efe4b7507
adityabelltom <aditya.belltom@gmail.com>
2026-05-04 00:54:20 +0530

    fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,8 @@ import allRouter from "./routes";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
@@ -21,8 +22,6 @@ app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-app.listen(Number(PORT), "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 
 export default app;
