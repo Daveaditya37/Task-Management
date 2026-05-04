6b22de8ea96d8a09cf6b673a140c20f51036cdd3
adityabelltom <aditya.belltom@gmail.com>
2026-05-04 10:28:56 +0530

    fixed

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,17 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const routes_1 = __importDefault(require("./routes"));
+const index_1 = __importDefault(require("./routes/index"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "*",
+}));
 app.use(express_1.default.json());
-app.use("/api", routes_1.default);
+app.use("/api", index_1.default);
 app.get("/", (req, res) => {
     res.send("Backend Running");
 });
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
 exports.default = app;
