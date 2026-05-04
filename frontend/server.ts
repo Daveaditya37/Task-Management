36475e00a6b754595336ae7d9a1767c7dc28188d
adityabelltom <aditya.belltom@gmail.com>
2026-05-04 11:50:54 +0530

    fixed

--- frontend/server.ts
+++ frontend/server.ts
@@ -12,7 +12,7 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(express.static(path.join(__dirname, "dist")));
 
 // SPA fallback (important for React Router)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
