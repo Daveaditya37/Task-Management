3cb653f7fba5e4269fe58d0b0186acf0a4a278dc
adityabelltom <aditya.belltom@gmail.com>
2026-05-04 12:17:17 +0530

    fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,10 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "https://ethara-task-management.up.railway.app",
-  credentials: true,
-}));
+app.use(cors()); 
 
 app.use(express.json());
 
